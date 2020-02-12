f7ae8cfba10f457ed602e86f1c77c1fb201b2649
Purnima Singh <purnimasingh@Purnimas-MacBook-Pro.local>
2020-02-12 15:18:14 +0530

    sankey chart

---
+++ build/precache-manifest.e8db89311b25de7d6eebeeab29bccf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c023b458013a51a0fa6957d626190d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643bc179c511fc1d72f7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d397812f76d278a23a1",
+    "url": "/static/js/2.051ad870.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.051ad870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643bc179c511fc1d72f7",
+    "url": "/static/js/main.d657c9db.chunk.js"
+  },
+  {
+    "revision": "6a877c112ec535eb776a",
+    "url": "/static/js/runtime-main.7e168d45.js"
+  }
+]);
